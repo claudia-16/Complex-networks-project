db8d13997d8ddac21a9eb9e525ae1a1855c447fc
Claudia <claudia.ghinato@studio.unibo.it>
2022-09-06 17:12:18 +0200

    Improved code with pylint

--- test_complex.py
+++ test_complex.py
@@ -7,24 +7,39 @@ Created on Mon Sep  5 11:44:32 2022
 
 #TESTING
 
+import random
 import pytest
 from hypothesis import given
 import hypothesis.strategies as st
 import numpy as np
-import random 
-import ipdb
+#import ipdb
 #from grappa import should, expect
-
-#from complex.py import triupper
 
 #%%
 
-# function for obtaining an upper triangular matrix (0s on the main diagonal)
-# the function overwrite the input matrix
 def triupper(prob, matrix):
+
+    ''''Function for obtaining an upper triangular matrix (0s on the main diagonal).
+        The values above the main diagonal are randomly 0 or 1, according to the probability "prob".
+        The function overwrites the input matrix
+           
+        Parameters
+        ----------
+        prob : positive float
+            Probability of having a link (value 1) in the upper triangular matrix
+
+        matrix : numpy ndarray
+            Input square matrix to be transformed
+
+        Returns
+        -------
+        numpy ndarray
+
+        '''
+
     if prob<0:
         raise ValueError("Probability must be >0")
-    row= matrix.shape[0] # number of rows 
+    row= matrix.shape[0] # number of rows
     col=matrix.shape[1]  # number of columns
     if row!=col:
         raise IndexError("The input matrix must be a square matrix")
@@ -35,16 +50,23 @@ def triupper(prob, matrix):
             if (i>j or i==j):
                 matrix[i][j]=0
             else:
-                if (random.uniform(0,1)> 1-prob): # probability of having a link is greater than not having it
+                # probability of having a link is greater than not having it
+                if random.uniform(0,1)> 1-prob:
                     matrix[i][j]=1
                 else:
                     matrix[i][j]=0
 
+#%%
+
 ### POSITIVE TESTS
 
-# Below: test for verifying that each element on or below the diagonal is 0, whatever (or almost) is the size of the matrix 
 @given(dim=st.integers(min_value=0, max_value=100))
 def test_below_diag_val(dim):
+    '''
+    Test verifying that each element on the main diagonal
+    or below it is 0, considering different sizes of the matrix
+
+    '''
     prob=0.8 #arbitrary value in [0,1]
     matrix= np.ones((dim,dim))
     triupper(prob, matrix)
@@ -52,35 +74,51 @@ def test_below_diag_val(dim):
         for j in range(i,dim-1): # row index
             assert matrix[j][i] ==0
 
-# Below: test for veryfying that values above the main diagonal are actually inserted
+
 @given(dim_bis=st.integers(min_value=0, max_value=100))
 def test_above_diag_val(dim_bis):
+    '''
+    Test verifying that above the main diagonal 1 values are actually inserted
+
+    '''
     prob=1
     matrix= np.zeros((dim_bis,dim_bis))
     triupper(prob, matrix)
-    assert np.sum(matrix)== (pow(dim_bis,2)-dim_bis)/2 
+    assert np.sum(matrix)== (pow(dim_bis,2)-dim_bis)/2
+
 
 
 ### NEGATIVE TESTS
 
-# If i give a negative value of probability, the function must raise an error
 @given(neg_prob=st.floats(min_value=-100, max_value=-0.1))
 def test_neg_prob(neg_prob):
+    '''
+    Test verifying an error is raised when the probability has a negative value
+
+    '''
     matrix=np.ones((3,3))
     with pytest.raises(ValueError):
         triupper(neg_prob,matrix)
 
 
-# The input must be a square matrix
 def test_sq_matrix():
+    '''
+    Test verifying an error is raised when the input matrix is not a square matrix
+
+    '''
     prob=0.8
     matrix=np.array([[1,2,3,4],
-                    [5,6,7,8]]) 
+                    [5,6,7,8]])
     with pytest.raises(IndexError):
         triupper(prob,matrix)
-        
+
+
 # The input matrix must be a numpy ndarray
 def test_input_type():
+    '''
+    Test verifying an error is raised when the input matrix is not a numpy ndarray
+
+    '''
     prob=0.8
     matrix_tuple=(1,2,3,4)
     matrix_list=[1,2,3,4,5]
@@ -95,20 +133,29 @@ def test_input_type():
         triupper(prob, matrix_n)
 
 
+#%%
+
 ### PROPERTY TESTS
 
-# Test for verifying that the function does not change the matrix size
 @given(dim_tris=st.integers(min_value=0, max_value=100))
 def test_same_dim(dim_tris):
+    '''
+    Test verifying the function does not change the size of the matrix
+
+    '''
     prob=0.8
     matrix= np.ones((dim_tris,dim_tris))
     triupper(prob, matrix)
     assert np.size(matrix,axis=0)==dim_tris
     assert np.size(matrix,axis=1)==dim_tris
-    
 
-# The output matrix is of the same type of the input matrix (i.e., it is a numpy ndarray)
+
+
 def test_same_type():
+    '''
+    Test verifying the output matrix is of the same type of the input matrix (numpy ndarray)
+
+    '''
     prob=0.8
     matrix= np.ones((5,5))
     type_in= type(matrix)
@@ -117,3 +164,4 @@ def test_same_type():
     assert type_out==type_in
 
 
+#%%
